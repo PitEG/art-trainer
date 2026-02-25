aa18d11e7324fabeea2a527a500592c359b4db01
Enrique Gan <enriqueganpit@gmail.com>
2026-02-25 12:47:36 -0800

    feat: oddcolor has progressive difficulty now

--- src/OddColor/OddColor.tsx
+++ src/OddColor/OddColor.tsx
@@ -3,23 +3,27 @@ import { useState } from "react";
 import Grid from "./Grid";
 import "./OddColor.scss";
 
+const SOFT_LEVEL_CAP = 50;
+
 const OddColor = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
+
   // select random color
   const randomColor = {
     l: Math.random() * 0.8 + 0.1, // l is between [0.1,0.9]
     a: Math.random() * 1.8 - 0.9, // a is between [-0.8,0.8]
     b: Math.random() * 1.8 - 0.9, // b is between [-0.8,0.8]
   };
+  const difficultyModifier = 0.05 * (1.05 - score / SOFT_LEVEL_CAP);
   const offset = (x: number, offset: number) => {
     const sign = Math.random() > 0.5 ? 1 : -1;
     return x + sign * offset;
   };
   const oddColor = {
-    l: offset(randomColor.l, 0.05),
-    a: offset(randomColor.a, 0.05),
-    b: offset(randomColor.b, 0.05),
+    l: offset(randomColor.l, difficultyModifier),
+    a: offset(randomColor.a, difficultyModifier),
+    b: offset(randomColor.b, difficultyModifier),
   };
 
   const correct = () => {
