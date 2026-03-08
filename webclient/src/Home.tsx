aeb69532b6f6a71c064b8b980f0f6ab24ebe7a34
Enrique Gan <enriqueganpit@gmail.com>
2026-03-07 21:07:40 -0800

    fix: compile error fix

--- webclient/src/Home.tsx
+++ webclient/src/Home.tsx
@@ -1,4 +1,4 @@
-import { Container, Card, Button } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import { OddColorPreview } from "./OddColor/OddColor";
 import { Link } from "react-router";
 
